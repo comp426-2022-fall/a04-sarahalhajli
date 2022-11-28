4f56c3ad6807169ea23ab5c342b329e033341ae5
Sarah Alhajli <sarahalh@live.unc.edu>
2022-11-28 14:04:53 -0500

    Fixing errors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
-import { roll } from '../lib/roll.js'
+import { roll } from './lib/roll.js'
 import express from 'express'
 
 const args = minimist(process.argv.slice(2));
 
-const express = require('express')
+//const express = require('express')
 const app = express()
 
 const port = args.port || 5000;
@@ -13,8 +13,13 @@ const port = args.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.listen(port, () => {
+	console.log("Server listening on port" + port)
+})
+
 app.get('/app/', (req, res) => {
 	res.send('200 OK')
+	res.end()
 })
 
 app.get('/app/roll/', (req, res) => {
@@ -50,17 +55,7 @@ app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
 	res.end()
 })
 
-app.use((err, req, res, next) => {
-  console.error(err.stack)
+app.use(function(req, res) {
   res.status(404).send('404 NOT FOUND')
 })
 
-/*app.get('', (req, res, next) => {
-	res.status(404).send('404 NOT FOUND')
-})*/
-
-
-app.listen(port, () => {
-	console.log("Server listening on port" + port)
-
-})
